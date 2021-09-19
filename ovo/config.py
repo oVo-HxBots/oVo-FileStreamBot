c70cbe9509f038112059d86c583766f50778a5c2
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-09-19 06:40:50 +0530

    Update config.py

--- ovo/config.py
+++ ovo/config.py
@@ -9,17 +9,17 @@ load_dotenv()
 
 
 class Config(object):
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = "924859"
+    API_HASH = "a4c9a18cf4d8cb24062ff6916597f832"
+    BOT_TOKEN = "1670920423:AAGNpQwyLUFDRPdqQZQtWomuI_uGC6nfyso"
     SESSION_NAME = str(getenv('SESSION_NAME', 'oVo_FileStreamBot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '3'))
-    LOG_CHANNEL = int(getenv('LOG_CHANNEL'))
+    LOG_CHANNEL = "-1001364200595"
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    OWNER_ID = int(getenv('OWNER_ID', '1234567890'))
-    NO_PORT = bool(getenv('NO_PORT', False))
+    BIND_ADRESS = "filestream.rehost.tech"
+    OWNER_ID = "754495556"
+    NO_PORT = "False"
     APP_NAME = None
     if 'DYNO' in environ:
         ON_HEROKU = True
@@ -27,6 +27,6 @@ class Config(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU else APP_NAME+'.herokuapp.com'
-    DATABASE_URL = str(getenv('DATABASE_URL'))
-    JOIN_CHANNEL = str(getenv('JOIN_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))
+    DATABASE_URL = "mongodb+srv://user:user@cluster0.x7biw.mongodb.net/filestreambot"
+    JOIN_CHANNEL = "hxbots"
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS",)).split()))
