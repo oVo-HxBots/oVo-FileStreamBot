a13909cba1b90eaeac16051389117cfff8e5192a
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-09-20 06:17:31 +0530

    Update config.py

--- ovo/config.py
+++ ovo/config.py
@@ -12,11 +12,11 @@ class Config(object):
     API_ID = "924859"
     API_HASH = "a4c9a18cf4d8cb24062ff6916597f832"
     BOT_TOKEN = "1670920423:AAGNpQwyLUFDRPdqQZQtWomuI_uGC6nfyso"
-    SESSION_NAME = str(getenv('SESSION_NAME', 'oVo_FileStreamBot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '3'))
+    SESSION_NAME = "oVo_FileStreamBot"
+    SLEEP_THRESHOLD = "10"
+    WORKERS = "3"
     LOG_CHANNEL = "-1001364200595"
-    PORT = int(getenv('PORT', 8080))
+    PORT = "8080"
     BIND_ADRESS = "filestream.rehost.tech"
     OWNER_ID = "754495556"
     NO_PORT = "False"
@@ -29,4 +29,4 @@ class Config(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU else APP_NAME+'.herokuapp.com'
     DATABASE_URL = "mongodb+srv://user:user@cluster0.x7biw.mongodb.net/filestreambot"
     JOIN_CHANNEL = "hxbots"
-    BANNED_CHANNELS = "-1001378693486"
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))
